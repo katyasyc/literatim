93797491b582d2796feb5523f0f5fbf678790397
katyasyc <katyasyc@gmail.com>
2017-02-07 09:23:18 -0500

    other files

---
+++ app/src/main/java/org/troi/literatim/LatinKeyboardView.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2011-2015 The Literatim authors
+ * Copyright (C) 2008-2009 Google Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.troi.literatim;
+
+import android.content.Context;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.inputmethodservice.Keyboard.Key;
+import android.view.inputmethod.InputMethodManager;
+import android.util.AttributeSet;
+
+public class LatinKeyboardView extends KeyboardView {
+
+    static final int KEYCODE_OPTIONS = -100;
+
+    public LatinKeyboardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LatinKeyboardView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected boolean onLongPress(Key key) {
+        //if (key.codes[0] == Keyboard.KEYCODE_CANCEL) {
+        //    getOnKeyboardActionListener().onKey(KEYCODE_OPTIONS, null);
+        //    return true;
+        //} else {
+
+        // long press on spacebar - show the input picker
+        if (key.codes[0] == 32) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.showInputMethodPicker();
+            return false;
+        }
+        return super.onLongPress(key);
+        //}
+    }
+}
